89f85c466c3ed9ca2e325600f48608a6f883c15f
Dmitriy Komarov <dkomarov@samokat.ru>
2021-04-14 15:44:44 +0300

    Adds Validated annotation in controller

--- src/main/java/com/github/komarovd95/widgetstore/application/controller/WidgetsController.java
+++ src/main/java/com/github/komarovd95/widgetstore/application/controller/WidgetsController.java
@@ -19,6 +19,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -37,6 +38,7 @@ import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/widgets")
+@Validated
 @Tag(name = "Widgets API", description = "REST API for interaction with widgets")
 public class WidgetsController {
 
